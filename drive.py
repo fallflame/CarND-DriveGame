4cad2d7da293f8bc4cdf4871641b05c86347bef6
Yan Xu <Yan Xu>
2017-02-07 19:15:39 -0500

    manage throttle

--- drive.py
+++ drive.py
@@ -42,7 +42,7 @@ def telemetry(sid, data):
     steering_angle = float(model.predict(transformed_image_array, batch_size=1))
     # The driving model currently just outputs a constant throttle. Feel free to edit this.
 	
-    throttle = max(0.1, -0.15/0.05 * abs(steering_angle) + 0.35)
+    throttle = max(0.1, -4 * abs(steering_angle) + 0.35)
     #print(steering_angle, throttle)
     send_control(steering_angle, throttle)
 
